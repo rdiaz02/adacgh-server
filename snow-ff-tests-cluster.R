2fbea97e23f49271ec8d46e12d2d44f3be5b66ff
ramon diaz-uriarte <ramon@xantusia>
2009-11-13 19:46:58 +0100

    Former-commit-id: 4045046fadbeaba7c603a280326e8f46828c6c6b

--- snow-ff-tests-cluster.R
+++ snow-ff-tests-cluster.R
@@ -26,13 +26,9 @@ library(ff)
 ## save(file = "cghData.RData", cghData)
 ## rm(chromData)
 ## rm(cghData)
+## rm(cghData2)
 
 ## rm(list = ls())
-
-
-
-
-
 
 
 library(snowfall)
@@ -47,153 +43,15 @@ sfExport("mydir")
 setwd(mydir)
 sfClusterEval(setwd(mydir))
 
-
-
-
-
-
-
-
 ## source("/home/ramon/bzr-local-repos/adacgh2/R-packages/ADaCGH/R/ADaCGH-2.R")
 
 
-
-
-getCGHValue <- function(cghRDataName, array) {
-  nmobj <- load(cghRDataName)
-  open(get(nmobj, inherits = FALSE), readonly = TRUE)
-  tmp <- get(nmobj, inherits = FALSE)[, array]
-  close(get(nmobj, inherits = FALSE))
-  return(tmp)
-}
-
-getChromValue <- function(chromRDataName) {
-  nmobj <- load(chromRDataName)
-  open(get(nmobj, inherits = FALSE), readonly = TRUE)
-  tmp <- get(nmobj, inherits = FALSE)[]
-  close(get(nmobj, inherits = FALSE))
-      return(tmp)
-}
-
-getffObj <- function(RDataName, silent = FALSE) {
-  nmobj <- load(RDataName, env = parent.frame())
-  if(!silent) {
-    cat("\n Making an assignment in the calling environment!!! \n")
-    cat("We just created (or overwrote)", nmobj, "\n")
-    cat("Don't forget to close", nmobj, "\n")
-  }
-  open(get(nmobj, inherits = FALSE, envir = parent.frame()), readonly = TRUE)
-  return(nmobj)
-}
+## upload the code somehow!! sourcing, C-c C-r, whatever
 
 
 
-
-ffListOut <- function(smoothedVal, stateVal) {
-  pattern <- paste(getwd(), paste(sample(letters, 4), collapse = ""),
-                   sep = "/")
-  smoothed <- ff(smoothedVal,
-                 vmode = "double",
-                 pattern = pattern)
-  state <- ff(stateVal,
-              vmode = "short",
-              pattern = pattern)
-  close(smoothed)
-  close(state)
-  return(list(smoothed = smoothed,
-              state = state))
-}
-
-internalGLAD <- function(index, cghRDataName, chromRDataName, nvalues) {
-  ##  cghvalues <- getCGHval(cghRDataName, index)
-  ##  chromvalues <- getChromval(chromRDataName)
-  nodeWhere()
-  tmpf <- list(profileValues = data.frame(
-                 LogRatio = getCGHValue(cghRDataName, index),
-                 PosOrder = 1:nvalues,
-                 Chromosome = getChromValue(chromRDataName)))
-  class(tmpf) <- "profileCGH"
-  outglad <- glad.profileCGH(tmpf)
-  return(ffListOut(outglad$profileValues$Smoothing,
-                   outglad$profileValues$ZoneGNL))
-}
-
-
-
-
-pSegmentGLAD <- function(cghRDataName, chromRDataName, ...) {
-  
-  ## check appropriate class of objects
-  
-  ## stop.na.inf(x)
-  ## stop.na.inf(chrom.numeric)
-  ## warn.too.few.in.chrom(chrom.numeric)
-  
-  require("GLAD") || stop("Package not loaded: GLAD")
-  
-  nameCgh <- getffObj(cghRDataName, silent = TRUE)
-  nameChrom <- getffObj(chromRDataName, silent = TRUE)
-  arrayNames <- colnames(get(nameCgh))
-  
-  rle.chr <- intrle(as.integer(get(nameChrom))[])
-  
-  chromValues <- rle.chr$values
-  nchrom <- length(chromValues)
-  narrays <- ncol(get(nameCgh))
-  nvalues <- nrow(get(nameCgh))
-  close(get(nameCgh))
-  close(get(nameChrom))
-  
-  outsf <- sfClusterApplyLB(1:narrays,
-                            internalGLAD,
-                            cghRDataName, chromRDataName, nvalues)
-  
-  browser()
-  mysize(outsf)
-  browser()
-  return(outToffdf(outsf, arrayNames))
-}
-
-
-
-nodeWhere <- function() {
-  fn <- paste("nodeWhere", paste(sample(letters,8), sep = "", collapse = ""),
-              sep = "")
-  sink(file = fn)
-  date()
-  cat("\n HOSTNAME IS ")
-  print(system("hostname", intern = TRUE))
-  sink()
-}
-
-
-outToffdf <- function(out, arrayNames) {
-  nelem <- length(out)
-  if(is.null(arrayNames))
-    arrayNames <- paste("A", 1:nelem, sep = "")
-  ## this is horrible, but I can't get it to work otherwise
-  p1 <- paste("outSmoothed <- ffdf(",
-              paste(arrayNames, " = out[[", 1:nelem, "]]$smoothed", sep = "",
-                    collapse = ", "),
-              ")")
-  p2 <- paste("outState <- ffdf(",
-              paste(arrayNames, "= out", "[[", 1:nelem, "]]$state", sep = "",
-                    collapse = ", "),
-              ")")
-  eval(parse(text = p1))
-  eval(parse(text = p2))
-  colnames(outSmoothed) <- colnames(outState) <- arrayNames
-  return(list(outSmoothed = outSmoothed, outState = outState))
-}
-
-
-mysize <- function(x) {
-  cat("\n Size of object ",
-      deparse(substitute(x)), ": ")
-  print(object.size(x), units = "M")
-}
-
-sfExport("getChromValue", "getCGHValue", "ffListOut", "internalGLAD", "nodeWhere")
+sfExport("getChromValue", "getCGHValue", "ffListOut", "internalGLAD", "sizesobj",
+         "nodeWhere", "gcmessage")
 
 ## following doesn't work.
 #sfClusterEval(source("/home/ramon/bzr-local-repos/adacgh2/R-packages/ADaCGH/R/ADaCGH-2.R"))
